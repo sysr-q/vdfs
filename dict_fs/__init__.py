81fe5b0e57ff89cfcb5603466d7cf3fe04129000
plausibility <chris@gibsonsec.org>
2013-03-12 19:30:38 +1100

    Updated to a more usable interface.

--- dict_fs/__init__.py
+++ dict_fs/__init__.py
@@ -4,6 +4,14 @@ FS_BLOB = "__DICT_FS_BLOB__"
 FS_FILE = "__DICT_FS_FILE__"
 FS_DIR = "__DICT_FS_DIR__"
 
+CODES = {
+    0: "All according to plan",
+    1: "change_dir(): given non-string as directory name",
+    2: "change_dir(): already in root directory",
+    3: "change_dir(): directory non-existent",
+    4: "change_dir(): given directory name isn't a directory"
+}
+
 
 class dict_fs(object):
     def __init__(self, fs=None, env=None):
@@ -23,59 +31,63 @@ class dict_fs(object):
 
         self.current_dir = []
 
+        self.root_name = "/" # C:\, whatever.
+        self.sep = "/"
+
     def current_dictionary(self):
         d = self.fs[""]
         for key in self.current_dir:
             d = d[key]
         return d
 
-    def ls(self, args):
-        print "Contents of directory:", "/" + ("/".join(self.current_dir)) + ":"
-        for i in self.current_dictionary():
-            print i
+    def list_dir(self, dir_name=None):
+        dir_list = []
+        dir_list.append("Contents of directory: " +  self.root_name + (self.sep.join(self.current_dir)) + ":")
+        dir_list.extend([i for i in self.current_dictionary()])
+        return dir_list
 
-    def cd(self, args):
-        if len(args) != 1:
-            print "Usage: cd <directory>"
-            return
+    def is_root_dir(self):
+        return len(self.current_dir) == 0
 
-        if args[0] == "..":
-            if len(self.current_dir) == 0:
-                print "Can't go above root."
+    def change_dir(self, dir_name):
+        if not isinstance(dir_name, basestring):
+            return 1#CODE
+
+        if dir_name == "..":
+            if self.is_root_dir():
+                return 2#CODE
             else:
                 self.current_dir.pop()
-            return
-        elif args[0] not in self.current_dictionary():
-            print "Directory", args[0], "not found"
-            return
+            return True
+        elif dir_name not in self.current_dictionary():
+            return 3#CODE
         
         # The thing we're interested in "cd"ing into.
-        dive_into = self.current_dictionary()[args[0]]
+        dive_into = self.current_dictionary()[dir_name]
 
         if isinstance(dive_into, dict):
-            self.current_dir.append(args[0])
+            self.current_dir.append(dir_name)
+            return 0#CODE
         else:
-            print "Not a directory:", args[0]
+            return 4#CODE
 
-    def mkdir(self, args):
-        if len(args) != 1:
-            print "Usage: mkdir <directory>"
-            return
-        self.current_dictionary()[args[0]] = {}
+    def make_dir(self, dir_name):
+        self.current_dictionary()[dir_name] = {}
+        return True
 
-    def info(self, args):
+    def touch(self, filename, contents=None):
+        if not contents:
+            contents = ""
+        self.current_dictionary()[filename] = contents
+        return True
+
+    def print_debug_info(self, args):
         print "self.fs:"
         print pprint.pprint(self.fs)
         print "self.current_dictionary():"
         print self.current_dictionary()
         print "self.current_dir:"
         print self.current_dir
-
-    def handle(self, params):
-        commands = {'ls': self.ls, 'cd': self.cd, 'mkdir': self.mkdir, 'info': self.info}
-        cmd = params.split()[0]
-        if cmd in commands:
-            commands[cmd](params.split()[1:])
 
 # Semi-implemented Debian-like FS dictionary.
 pre_fs = {
@@ -89,12 +101,10 @@ pre_fs = {
             "sda1": FS_BLOB
         },
         "etc": {
-            "hosts": "\n".join(
-                [
+            "hosts": "\n".join([
                     "127.0.0.1 localhost",
                     "::1 ip6-localhost"
-                ]
-            )
+                ])
         },
         "home": {
             "user": {"docs": {}, "git": {}}
@@ -107,10 +117,23 @@ pre_fs = {
 }
 
 if __name__ == "__main__":
+    # Really bad examples of use.
+    
     fs = dict_fs(fs=pre_fs)
-    while True:
-        raw = raw_input("> ")
-        if raw == "break":
-            break
-        fs.handle(raw)
-    print "\nGoodbye!"
+    for f in fs.list_dir():
+        print f
+
+    print "-"*50
+    fs.change_dir("bin")
+    for f in fs.list_dir():
+        print f
+    cd = fs.change_dir("cp") # A file
+    print CODES[cd]
+
+    fs.make_dir("test")
+    fs.change_dir("test")
+
+    print "-"*50
+    fs.touch("example.txt")
+    for f in fs.list_dir():
+        print f
